fd2666683890ba3a093734e84406bf7daf113b0d
chv48v89rw-ui <terrogachie@gmail.com>
2026-06-29 19:08:04 +0300

    website

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,17 +136,17 @@ STATICFILES_DIRS = [
 ]
 
 # Media files (User uploads)
-import cloudinary
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-cloudinary.config(
-cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-api_key=os.environ.get("CLOUDINARY_API_KEY"),
-api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-)
-
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
-MEDIA_URL = "/media/"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Default primary key field type
